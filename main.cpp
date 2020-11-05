e1457f2a3b2df3d82313c08a8a41e32db54223a7
souaremsaidou <souaremsaidou@gmail.com>
2020-11-05 01:15:18 +0100

    create Server class

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <memory>
 #include <cstdlib>
 
+#include <iostream>
+
 //Project Includes
 #include <corvusoft/restbed/uri.hpp>
 #include <corvusoft/restbed/byte.hpp>
@@ -18,42 +20,62 @@
 //#include <catch.hpp>
 
 //System Namespaces
-using std::string;
 using std::multimap;
+using std::string;
 
 //Project Namespaces
-using restbed::Uri;
 using restbed::Bytes;
-using restbed::Request;
-using restbed::Session;
 using restbed::OK;
+using restbed::Request;
 using restbed::Resource;
-using restbed::Settings;
 using restbed::Service;
+using restbed::Session;
+using restbed::Settings;
+using restbed::Uri;
 
 //External Namespaces
 using namespace std;
 
-
-void get_method_handler( const shared_ptr< Session > session )
+void get_method_handler(const shared_ptr<Session> session)
 {
-    session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
+	session->close(OK, "Hello, World!", {{"Content-Length", "13"}});
 }
 
-int main( const int, const char** )
+class Server
 {
-    auto resource = make_shared< Resource >( );
-    resource->set_paths( { "/messages", "/queues/{id: [0-9]*}/messages" } );
-    resource->set_method_handler( "GET", get_method_handler );
-    
-    auto settings = make_shared< Settings >( );
-    settings->set_port( 1984 );
-    settings->set_default_header( "Connection", "close" );
-    
-    Service service;
-    service.publish( resource );
-    service.start( settings );
-    
-    return EXIT_SUCCESS;
-}
+	shared_ptr<Settings> _settings;
+	Service _service;
 
+public:
+	Server()
+	{
+		cout << "Starting server...\n";
+		auto resource = make_shared<Resource>();
+		resource->set_paths({"/messages", "/queues/{id: [0-9]*}/messages"});
+		resource->set_method_handler("GET", get_method_handler);
+
+		_settings = make_shared<Settings>();
+		_settings->set_port(1984);
+		_settings->set_default_header("Connection", "close");
+
+		_service.publish(resource);
+	}
+
+	void start()
+	{
+		//_service.start(_settings);
+	}
+
+	~Server()
+	{
+		cout << "Stopping server\n";
+		_service.stop();
+	}
+};
+
+int main(const int, const char **)
+{
+	Server server;
+	server.start();
+	return EXIT_SUCCESS;
+}
