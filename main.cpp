f4c95fdb850f14ec5194c190fbd03cbc311ebe6f
souaremsaidou <souaremsaidou@gmail.com>
2020-11-08 16:30:14 +0100

    libpqxx-dev cmake integration

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 //System Includes
+#include <thread>
 #include <map>
 #include <string>
 #include <memory>
 #include <cstdlib>
-#include <csignal>
 #include <iostream>
 
+#include <csignal>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <chrono>
+
 //Project Includes
+
+
+//External Includes
+//#include <catch.hpp>
 #include <corvusoft/restbed/uri.hpp>
 #include <corvusoft/restbed/byte.hpp>
 #include <corvusoft/restbed/request.hpp>
@@ -15,13 +25,12 @@
 #include <corvusoft/restbed/resource.hpp>
 #include <corvusoft/restbed/service.hpp>
 #include <corvusoft/restbed/settings.hpp>
-
-//External Includes
-//#include <catch.hpp>
+#include <pqxx/pqxx>
 
 //System Namespaces
 using std::multimap;
 using std::string;
+using std::thread;
 
 //Project Namespaces
 using restbed::Bytes;
@@ -35,6 +44,17 @@ using restbed::Uri;
 
 //External Namespaces
 using namespace std;
+using std::chrono::seconds;
+using namespace pqxx;
+
+// global
+// mutex
+
+//
+void ready_handler(Service &)
+{
+	fprintf(stderr, "Service PID is '%i'.\n", getpid());
+}
 
 void get_method_handler(const shared_ptr<Session> session)
 {
@@ -45,41 +65,64 @@ class Server
 {
 	shared_ptr<Settings> _settings;
 	Service _service;
+	shared_ptr<thread> _worker = nullptr;
 
 public:
 	Server()
 	{
-		cout << "Starting server...\n";
-		auto resource = make_shared<Resource>();
-		resource->set_paths({"/messages", "/queues/{id: [0-9]*}/messages"});
-		resource->set_method_handler("GET", get_method_handler);
-
-		_settings = make_shared<Settings>();
-		_settings->set_port(1984);
-		_settings->set_default_header("Connection", "close");
-
-		_service.publish(resource);
+		_worker = make_shared< thread >( [&this ] ( )
+		{
+            start( );
+        } );
 	}
 
 	void start()
 	{
-		_service.set_signal_handler( SIGINT, [this](const int signo) {
-			cout << "Received SIGINT signal number " << signo << '\n';
+		cout << "Starting server...\n";
+		auto resource = make_shared<Resource>();
+		resource->set_path("/fizzbuzz/");
+		resource->set_method_handler("GET", get_method_handler);
+
+		_settings = make_shared<Settings>();
+
+		// read variable argument list
+		_settings->set_port(1984);
+		_settings->set_default_header("Connection", "close");
+
+		_service.publish(resource);
+		
+		_service.set_ready_handler(ready_handler);
+		_service.set_signal_handler(SIGINT, [this](const int signo) {
+			fprintf(stderr, "Received SIGINT signal number '%i'.\n", signo);
 			_service.stop();
-		    });
+		});
+		_service.set_signal_handler(SIGTERM, [this](const int signo) {
+			fprintf(stderr, "Received SIGTERM signal number '%i'.\n", signo);
+			_service.stop();
+		});
 
 		_service.start(_settings);
+	}
+	
+	void stop()
+	{
+		_service.stop();
 	}
 
 	~Server()
 	{
 		cout << "Stopping server\n";
+		_worker->join( );
+		stop();
 	}
 };
 
 int main(const int, const char **)
 {
 	Server server;
-	server.start();
+	
+	std::this_thread::sleep_for( seconds( 1 ) );
+	
+	server.stop();
 	return EXIT_SUCCESS;
 }
