e9f203dc54d4a57b3db75d462ed55ae436612ebd
Saidou <57505334+souaremsaidou@users.noreply.github.com>
2020-11-10 16:52:44 +0100

    Fix ci, Add unit test for Fizzbuzz algorithm and linking against Google Test

---
+++ tests/fetch_git_repo.cmake
@@ -0,0 +1,45 @@
+# the following code to fetch a git repo at configure-time
+# is inspired by and adapted after https://github.com/dev-cafe/cmake-cookbook/blob/v1.0/chapter-04/recipe-03/cxx-example-3.5/
+# defines ${_project_name}_SOURCE_DIR and ${_project_name}_BINARY_DIR
+
+macro(fetch_git_repo _project_name _download_root _git_url _git_tag)
+
+  set(${_project_name}_SOURCE_DIR ${_download_root}/${_project_name}-src)
+  set(${_project_name}_BINARY_DIR ${_download_root}/${_project_name}-build)
+
+  # variables used configuring fetch_git_repo_sub.cmake
+  set(FETCH_PROJECT_NAME ${_project_name})
+  set(FETCH_SOURCE_DIR ${${_project_name}_SOURCE_DIR})
+  set(FETCH_BINARY_DIR ${${_project_name}_BINARY_DIR})
+  set(FETCH_GIT_REPOSITORY ${_git_url})
+  set(FETCH_GIT_TAG ${_git_tag})
+
+  configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/fetch_at_configure_step.in
+    ${_download_root}/CMakeLists.txt
+    @ONLY
+    )
+
+  # undefine them again
+  unset(FETCH_PROJECT_NAME)
+  unset(FETCH_SOURCE_DIR)
+  unset(FETCH_BINARY_DIR)
+  unset(FETCH_GIT_REPOSITORY)
+  unset(FETCH_GIT_TAG)
+
+  # configure sub-project
+  execute_process(
+    COMMAND
+      "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+    WORKING_DIRECTORY
+      ${_download_root}
+    )
+
+  # build sub-project which triggers ExternalProject_Add
+  execute_process(
+    COMMAND
+      "${CMAKE_COMMAND}" --build .
+    WORKING_DIRECTORY
+      ${_download_root}
+    )
+endmacro()
--- tests/fizzbuzz.cpp
+++ tests/fizzbuzz.cpp
@@ -1,6 +1,22 @@
 #include "fizzbuzz.hpp"
+#include "gtest/gtest.h"
 
-int main()
+#include <vector>
+
+int main(int argc, char **argv)
 {
-	return 0;
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+TEST(FizzbuzzTest, TestOk)
+{
+	std::vector<std::string> expected = {"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16"};
+	ASSERT_EQ(expected, fizzbuzz(3, 5, 16, "fizz", "buzz"));
+}
+
+TEST(FizzbuzzTest, TestkO)
+{
+	std::vector<std::string> expected = {"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16"};
+	ASSERT_NE(expected, fizzbuzz(3, 5, 16, "", ""));
 }
