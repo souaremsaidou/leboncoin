975598b0d2bc4fb90af770fac9dec6e352ef5fa7
souaremsaidou <souaremsaidou@gmail.com>
2020-11-14 23:35:21 +0100

    add command line parser and db.cfg

--- src/fizzbuzz/fizzbuzz.cpp
+++ src/fizzbuzz/fizzbuzz.cpp
@@ -1,28 +1,20 @@
 #include "fizzbuzz.hpp"
 
-std::vector<std::string> fizzbuzz(int int1, int int2, int limit, std::string str1, std::string str2)
-{
-	std::vector<std::string> values;
-	values.reserve(limit);
-	for (int i = 1; i < limit + 1; ++i)
-	{
-		if (i % int1 == 0 && i % int2 == 0)
-		{
-			values.push_back(str1 + str2);
-		}
-		else if (i % int1 == 0)
-		{
-			values.push_back(str1);
-		}
-		else if (i % int2 == 0)
-		{
-			values.push_back(str2);
-		}
-		else
-		{
-			values.push_back(std::to_string(i));
-		}
-	}
+std::vector<std::string> fizzbuzz(int int1, int int2, int limit,
+                                  std::string str1, std::string str2) {
+  std::vector<std::string> values;
+  values.reserve(limit);
+  for (int i = 1; i < limit + 1; ++i) {
+    if (i % int1 == 0 && i % int2 == 0) {
+      values.push_back(str1 + str2);
+    } else if (i % int1 == 0) {
+      values.push_back(str1);
+    } else if (i % int2 == 0) {
+      values.push_back(str2);
+    } else {
+      values.push_back(std::to_string(i));
+    }
+  }
 
-	return values;
+  return values;
 }
--- src/fizzbuzz/fizzbuzz.hpp
+++ src/fizzbuzz/fizzbuzz.hpp
@@ -3,4 +3,5 @@
 #include <string> // std::string, std::to_string
 #include <vector>
 
-std::vector<std::string> fizzbuzz(int int1, int int2, int limit, std::string str1, std::string str2);
+std::vector<std::string> fizzbuzz(int int1, int int2, int limit,
+                                  std::string str1, std::string str2);
